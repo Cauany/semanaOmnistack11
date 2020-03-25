4c3b6a20f18b852462f9b48fa6d2e943cdb47494
Cauany Souza <souza.cauany99@gmail.com>
2020-03-25 16:00:04 -0300

    criando as rotas de Logon e Register

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,7 +1,31 @@
 import React from 'react'
 
-import './styles.css'
+import './style.css'
+
+import { Link } from 'react-router-dom'
+import { FiArrowLeft} from 'react-icons/fi'
+
+import logoImg from '../../assets/logoImg'
 
 export default function Register() {
-    return <h1> Register </h1>
+    return (
+        <div className="register-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Be the Hero" />
+                    <h1> Cadastro </h1>
+                    <p> Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+                
+                    <Link className="back-link" to="/register">
+                    <FiArrowLeft size={16} color="#E02041" />
+                    Não tenho cadastro
+                </Link>
+                </section>
+
+                <form action="">
+
+                </form>
+            </div>
+        </div>
+    )
 }
